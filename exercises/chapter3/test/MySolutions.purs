a05cbf98044fa69abe75ae196f027f295a66e7af
Mickey Smith <smithmick14@gmail.com>
2023-10-05 13:50:18 -0400

    [chapter 3] findEntryByStreet'

--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -2,9 +2,9 @@ module Test.MySolutions (findEntryByStreet) where
 
 import Prelude
 
-import Data.Maybe (Maybe)
+import Data.AddressBook (Entry, AddressBook)
 import Data.List (head, filter)
-import Data.AddressBook (AddressBook, Entry)
+import Data.Maybe (Maybe)
 
 filterEntryByStreet :: String -> Entry -> Boolean
 filterEntryByStreet street entry = 
@@ -13,3 +13,6 @@ filterEntryByStreet street entry =
 findEntryByStreet :: String -> AddressBook -> Maybe Entry
 findEntryByStreet street book = 
   (head <<< filter (filterEntryByStreet street)) book
+
+findEntryByStreet' :: String -> AddressBook -> Maybe Entry
+findEntryByStreet' street = head <<< filter (eq street <<< _.address.street)
