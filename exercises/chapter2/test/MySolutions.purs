388fad4dcb1cf2bc53cd3b7e58ce7ca75b51796f
Mickey Smith <smithmick14@gmail.com>
2023-10-05 10:44:14 -0400

    [chapter 2] circleArea function

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -2,7 +2,10 @@ module Test.MySolutions where
 
 import Prelude
 
-import Data.Number (sqrt)
+import Data.Number (sqrt, pi)
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt ((w * w) + (h * h))
+
+circleArea :: Number -> Number
+circleArea r = pi * (r * r)
