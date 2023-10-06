936f8ee092c310bd26c5849d253025f117045d2f
Mickey Smith <smithmick14@gmail.com>
2023-10-06 15:04:54 -0400

    [chapter 4] isEven

--- exercises/chapter4/test/MySolutions.purs
+++ exercises/chapter4/test/MySolutions.purs
@@ -3,3 +3,7 @@ module Test.MySolutions where
 import Prelude
 
 -- Note to reader: Add your solutions to this file
+isEven :: Int -> Boolean
+isEven n = 
+  if n >= 0 then n `mod` 2 == 0
+  else isEven (-1 * n)
