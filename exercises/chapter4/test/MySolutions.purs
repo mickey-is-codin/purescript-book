42bd62fa1a7dcfb2499a61c6e2035df49af8193e
Mickey Smith <smithmick14@gmail.com>
2023-10-06 15:23:36 -0400

    [chapter 4] countEven

--- exercises/chapter4/test/MySolutions.purs
+++ exercises/chapter4/test/MySolutions.purs
@@ -2,8 +2,30 @@ module Test.MySolutions where
 
 import Prelude
 
+import Data.Array (head, tail, null)
+import Data.Maybe (fromMaybe)
+
 -- Note to reader: Add your solutions to this file
 isEven :: Int -> Boolean
 isEven n = 
   if n >= 0 then n `mod` 2 == 0
   else isEven (-1 * n)
+
+countEven :: Array Int -> Int
+countEven xs = 
+  if null xs then 0
+  else 
+    if isEven blah then 1 + countEven grug
+    else 0 + countEven grug
+      where
+        blah = fromMaybe 1 $ head xs
+        grug = fromMaybe [] $ tail xs
+
+countEven' :: Array Int -> Int
+countEven' xs = 
+  if null xs then 0
+  else oneIfEven blah + countEven grug
+    where
+      oneIfEven n = if isEven n then 1 else 0
+      blah = fromMaybe 1 $ head xs
+      grug = fromMaybe [] $ tail xs
