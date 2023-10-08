c58354e9f6d13d7bf081194c3c5066573f5fb106
Mickey Smith <smithmick14@gmail.com>
2023-10-08 17:23:45 -0400

    [chapter 5] sameCity

--- exercises/chapter5/test/MySolutions.purs
+++ exercises/chapter5/test/MySolutions.purs
@@ -2,6 +2,8 @@ module Test.MySolutions where
 
 import Prelude
 
+import Data.Person (Person)
+
 factorial :: Int -> Int
 factorial 0 = 1
 factorial 1 = 1
@@ -17,3 +19,7 @@ pascal :: Int -> Int -> Int
 pascal _ 0 = 1
 pascal 0 _ = 0
 pascal n k = pascal (n - 1) k + pascal (n - 1) (k - 1)
+
+sameCity :: Person -> Person -> Boolean
+-- sameCity person1 person2 = person1.address.city == person2.address.city
+sameCity { address: { city: city1 } } { address: { city: city2 } } = city1 == city2
