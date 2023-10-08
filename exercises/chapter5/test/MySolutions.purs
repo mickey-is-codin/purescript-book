4d58f1a1a744baa421b1949f10572640d263e6a0
Mickey Smith <smithmick14@gmail.com>
2023-10-08 16:53:54 -0400

    [chapter 5] binomial

--- exercises/chapter5/test/MySolutions.purs
+++ exercises/chapter5/test/MySolutions.purs
@@ -6,3 +6,9 @@ factorial :: Int -> Int
 factorial 0 = 1
 factorial 1 = 1
 factorial x = x * factorial (x - 1)
+
+binomial :: Int -> Int -> Int
+binomial _ 0 = 1
+binomial 0 _ = 0
+binomial n k | n < k = 0
+             | otherwise = factorial n / (factorial k * (factorial (n - k)))
